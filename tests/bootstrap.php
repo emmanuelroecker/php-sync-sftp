0acbc006f6d073ee2d58fe71f55a3b4a385eaa0e
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2015-08-13 19:56:14 +0200

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use GlSyncFtp\GlSyncFtp;
 
 $ftp = new GlSyncFtp(FTP_SERVER_HOST, FTP_SERVER_USER, FTP_SERVER_PASSWORD);
-$ftp->syncDirectory(
-    __DIR__ . '/delete',
-        'test',
-        function ($op, $path) {
-            echo 'delete ' . $path;
-        }
-);
+$ftp->syncDirectory(__DIR__ . '/delete', '/test');
 $ftp->disconnect();
+
 
