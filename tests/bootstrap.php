653a64561f43b8201b84682a289b7828154c547d
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-01-12 15:38:00 +0100

    create delete dir for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+mkdir(__DIR__ . '/tests/delete');
+
 use GlSyncFtp\GlSyncFtp;
 
 $ftp = new GlSyncFtp(FTP_SERVER_HOST, FTP_SERVER_USER, FTP_SERVER_PASSWORD);
